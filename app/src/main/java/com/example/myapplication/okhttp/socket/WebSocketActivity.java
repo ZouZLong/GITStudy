a5b36f279a8ed8d02f39aa4a5d53d0c3dce38b1e
zhouzhilong <1715011480@qq.com>
2021-03-01 13:59:49 +0800

    websocket

---
+++ app/src/main/java/com/example/myapplication/okhttp/socket/WebSocketActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.okhttp.socket;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.myapplication.BaseActivity;
+
+/**
+ * WebSocket长链接编码
+ */
+public class WebSocketActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
